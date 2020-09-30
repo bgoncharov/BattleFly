3dda456615cfb912d5fbbba47f9d7edf1d539986
Boris Goncharov <bora4bora@gmail.com>
2020-09-30 13:13:12 -0700

    background

---
+++ BattleFly/Background.swift
@@ -0,0 +1,21 @@
+//
+//  Background.swift
+//  BattleFly
+//
+//  Created by Boris Goncharov on 9/30/20.
+//  Copyright © 2020 Boris Goncharov. All rights reserved.
+//
+
+import SpriteKit
+
+class Background: SKSpriteNode {
+    
+    static func populateBackground(at point: CGPoint) -> Background {
+        
+        let background = Background(imageNamed: "background")
+        background.position = point
+        background.zPosition = 0
+        
+        return background
+    }
+}
