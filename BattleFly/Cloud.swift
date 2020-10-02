0c680eba800a85a5d2f3ad793c9b01fb9d8cf7fe
Boris Goncharov <bora4bora@gmail.com>
2020-10-02 15:41:45 -0700

    Clouds and islands action added

--- BattleFly/Cloud.swift
+++ BattleFly/Cloud.swift
@@ -21,6 +21,7 @@ final class Cloud: SKSpriteNode, GameBackgroundSpritable {
         cloud.setScale(randomScaleFactor)
         cloud.position = point
         cloud.zPosition = 10
+        cloud.run(move(from: point))
         
         return cloud
     }
@@ -38,5 +39,14 @@ final class Cloud: SKSpriteNode, GameBackgroundSpritable {
         let randomNumber = CGFloat(distribution.nextInt()) / 10
         
         return randomNumber
+    }
+    
+    fileprivate static func move(from point: CGPoint) -> SKAction {
+        
+        let movePoint = CGPoint(x: point.x, y: -200)
+        let moveDistance = point.y + 200
+        let movementSpeed: CGFloat = 15.0
+        let duration = moveDistance / movementSpeed
+        return SKAction.move(to: movePoint, duration: TimeInterval(duration))
     }
 }
--- BattleFly/Island.swift
+++ BattleFly/Island.swift
@@ -18,11 +18,12 @@ final class Island: SKSpriteNode, GameBackgroundSpritable {
         island.position = point
         island.zPosition = 1
         island.run(rotateForRandomAngl())
+        island.run(move(from: point))
         
         return island
     }
 
-    static func configureIslandName() -> String {
+    fileprivate static func configureIslandName() -> String {
         let distribution = GKRandomDistribution(lowestValue: 1, highestValue: 4)
         let randomNumber = distribution.nextInt()
         let imageName = "is" + "\(randomNumber)"
@@ -30,17 +31,26 @@ final class Island: SKSpriteNode, GameBackgroundSpritable {
         return imageName
     }
     
-    static var randomScaleFactor: CGFloat {
+    fileprivate static var randomScaleFactor: CGFloat {
         let distribution = GKRandomDistribution(lowestValue: 1, highestValue: 10)
         let randomNumber = CGFloat(distribution.nextInt()) / 10
         
         return randomNumber
     }
     
-    static func rotateForRandomAngl() -> SKAction {
+    fileprivate static func rotateForRandomAngl() -> SKAction {
         let distribution = GKRandomDistribution(lowestValue: 0, highestValue: 360)
         let randomNumber = CGFloat(distribution.nextInt())
         
         return SKAction.rotate(toAngle: randomNumber * CGFloat(Double.pi / 100), duration: 0)
+    }
+    
+    fileprivate static func move(from point: CGPoint) -> SKAction {
+        
+        let movePoint = CGPoint(x: point.x, y: -200)
+        let moveDistance = point.y + 200
+        let movementSpeed: CGFloat = 10.0
+        let duration = moveDistance / movementSpeed
+        return SKAction.move(to: movePoint, duration: TimeInterval(duration))
     }
 }
